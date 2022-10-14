7ac0df2aa683451017b4a657686e9c2e62b82ba1
Remix Workspace <>
2022-10-14 11:02:51 -0400

    Organization Updates

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -13,33 +13,57 @@ contract Lottery {
     // Global switch to turn the lottery on and off.
     bool isContractEnabled;
 
-    address payable constant zeroAddress = payable(0x0000000000000000000000000000000000000000);
+    // The operator owns this contract and is responsible for running the lottery. They must fund this contract with gas, and in return they will receive a cut of each prize.
     //address payable constant operatorAddress = payable(0x1761DF124EC3bADb17Ef3B02167D068f3E542aC9);
     address payable constant operatorAddress = payable(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
-    uint constant operatorCut = 10; // int between 0 and 100 representing the percentage of the pot that the operator takes every game.
 
-    uint totalPrize;
+    // The zero address can never play since it has no owner. We just use it for cases where there are no valid players.
+    address payable constant zeroAddress = payable(0x0000000000000000000000000000000000000000);
 
+    // An integer between 0 and 100 representing the percentage of the "playerPrizePool" amount that the operator takes every game.
+    // Note that the player always receives the entire "bonusPrizePool" amount.
+    uint constant operatorCut = 10;
+
+    /* To ensure the safety of player money, the contract balance is accounted for by splitting it into three different places:
+        // contractFunds - The money used to pay for gas. The operator can add or remove money at will.
+        // playerPrizePool - The money players have paid to purchase tickets. The operator gets a cut of each prize automatically, but otherwise they cannot add or remove funds.
+        // bonusPrizePool - The money that the operator has optionally added to "sweeten the pot" and provide more prize money. The operator can add funds but cannot remove them.
+    */
+    uint contractFunds;
+    uint playerPrizePool;
+    uint bonusPrizePool;
+
+    // Variables to keep track of who is playing and how many tickets they have.
     mapping(address => bool) public map_isPlaying;
     address payable[] public list_playerAddress;
 
+    /*
+        Standard Contract Functions
+    */
+
     constructor() payable {
+        // When deploying this contract, initial funds should be paid to allow for smooth lottery operation.
         isContractEnabled = true;
     }
 
-    function requireContractEnabled() public view {
-        require(isContractEnabled, "This contract is currently disabled.");
+    receive() external payable {
+        // If a player sends money, then give them tickets. If the operator sends money, then add it to the contract funds.
+        if(msg.sender == operatorAddress) {
+            requireContractEnabled();
+
+            contractFunds += msg.value;
+            //addContractFunds() // Can we call this without paying again?
+        }
+        else {
+            playerPrizePool += msg.value;
+            registerAddress(payable(msg.sender));
+            //fundLottery() // Can we call this without paying again?
+        }
     }
 
-    function fundLottery() external payable {
-        // When addresses pay the contract, they are entered into the lottery.
-        // If they sent too much, return the excess amount.
-        // If they have already entered the lottery, error so the transfer can be reverted.
-        requireContractEnabled();
-
-        totalPrize += msg.value;
-        registerAddress(payable(msg.sender));
-    }
+    /*
+        Lottery Functions
+    */
 
     function registerAddress(address payable playerAddress) public {
         // If address is already playing, we need to error. An address can only enter the lottery once.
@@ -84,44 +108,28 @@ contract Lottery {
         }
         else {
             // Give the lottery operator their cut of the pot, and then give the rest to the winner.
-            uint operatorPrize = totalPrize * operatorCut / 100;
-            uint winnerPrize = totalPrize - operatorPrize;
-            totalPrize = 0;
+            uint operatorPrize = playerPrizePool * operatorCut / 100;
+            uint winnerPrize = bonusPrizePool + playerPrizePool - operatorPrize;
+            playerPrizePool = 0;
+            bonusPrizePool = 0;
             operatorAddress.transfer(operatorPrize);
             winningAddress.transfer(winnerPrize);
         }
     }
 
-    function fundContract() external payable {
-        // Directly fund the contract. This does not add to the prize or enter any addresses into the lottery.
-        // This should only be called by the lottery operator to give the contract gas.
-    }
-
-    function withdrawAll() public {
-        // Transfer the entire contract balance to the operator.
-        operatorAddress.transfer(getBalance());
-    }
-
-    function withdraw(uint amount) public {
-        // Transfer an amount from the contract balance to the operator.
-        uint balance = getBalance();
-        require(amount <= balance, string.concat("The amount ", Strings.toString(amount), " is greater than the contract balance ", Strings.toString(balance)));
-        operatorAddress.transfer(amount);
-    }
-
-    function getPrize() public view returns (uint) {
-        return totalPrize;
-    }
-
-    function getBalance() public view returns (uint) {
-        return address(this).balance;
-    }
+    /*
+        RNG Functions
+    */
 
     function randomInt(uint N) public view returns (uint) {
         // Generate a random integer 0 <= n < L.
         uint randomHash = uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp)));
         return randomHash % N;
     }
+
+    /*
+        Control Functions
+    */
 
     function enableContract() public {
         // Enable the ability for players to enter the lottery.
@@ -132,6 +140,80 @@ contract Lottery {
         // Disable the ability for players to enter the lottery.
         isContractEnabled = false;
     }
+
+    function requireContractEnabled() public view {
+        require(isContractEnabled, "This contract is currently disabled.");
+    }
+
+    /*
+        Funding Functions
+    */
+
+    function getContractFunds() external view returns (uint) {
+        return contractFunds;
+    }
+
+    function addContractFunds() external payable {
+        // Directly fund the contract. This does not add to the prize or enter any addresses into the lottery.
+        // This should only be called by the lottery operator to give the contract gas.
+        contractFunds += msg.value;
+    }
+
+    function removeContractFunds(uint amount) public {
+        // Transfer an amount from the contract balance to the operator.
+        uint balance = getContractBalance();
+        require(amount <= balance, string.concat("The amount ", Strings.toString(amount), " is greater than the contract balance ", Strings.toString(balance)));
+        contractFunds -= amount;
+        operatorAddress.transfer(amount);
+    }
+
+    function removeAllContractFunds() public {
+        // Transfer the entire contract balance to the operator.
+        contractFunds = 0;
+        operatorAddress.transfer(getContractBalance());
+    }
+
+    function getPlayerPrizePool() external view returns (uint) {
+        return playerPrizePool;
+    }
+
+    function fundLottery() external payable {
+        // When addresses pay the contract, they are entered into the lottery.
+        // If they sent too much, return the excess amount.
+        // If they have already entered the lottery, error so the transfer can be reverted.
+        requireContractEnabled();
+
+        playerPrizePool += msg.value;
+        registerAddress(payable(msg.sender));
+    }
+
+    function getBonusPrizePool() external view returns (uint) {
+        return bonusPrizePool;
+    }
+
+    function addBonusPrizePool() external payable {
+        // Add funds to the bonus prize pool.
+        bonusPrizePool += msg.value;
+    }
+
+    function getContractBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function getAccountedContractBalance() public view returns (uint) {
+        return contractFunds + playerPrizePool + bonusPrizePool;
+    }
+
+    function getExtraContractBalance() external view returns (uint) {
+        // Returns the amount of "extra" funds this contract has. This should usually be zero, but may be more if funds are sent here in ways that cannot be accounted for.
+        // For example, a coinbase transaction or another contract calling "selfdestruct" could send funds here without passing through the "receive" function for proper accounting.
+        assert(getContractBalance() >= getAccountedContractBalance());
+        return getContractBalance() - getAccountedContractBalance();
+    }
+
+    /*
+        Utility Functions
+    */
 
     /*
     function toString(address account) public pure returns(string memory) {
